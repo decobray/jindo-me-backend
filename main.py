c47ca783238e9e015a58558ff515314f5b987ed5
decobray <33930183+decobray@users.noreply.github.com>
2023-01-21 01:59:18 +0900

    Update main.py

--- main.py
+++ main.py
@@ -67,5 +67,5 @@ def subscribeStibee(form : SubscribeForm):
 
 
 #  at last, the bottom of the file/module
-# if __name__ == "__main__":
-#     uvicorn.run(app, host="127.0.0.1", port=8080)
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=8080)
